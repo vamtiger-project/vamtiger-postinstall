4329bd4ce8fdb14ef52b24177749a02683a9f95c
James-Alex Matthews <vamtiger@vamtiger.com>
2019-03-19 07:02:01 +0200

    vamtiger-node-typescript-commit

--- source/install.ts
+++ source/install.ts
@@ -6,16 +6,11 @@ const { install } = InstallScript;
 const { node_modules } = FolderName;
 
 export default async function ({ workingDirectory, dependencies, devDependencies }: IInstall) {
-    const folderContent = await getFolderContent(workingDirectory)
-        .then(folderContent => new Set(folderContent));
     const bashOptions = {
         cwd: workingDirectory
     };
-    const installDependencies = [
-        `cd ${workingDirectory};`,
-        dependencies && `${install} ${dependencies}` || `${install} --prefix ${workingDirectory} ${devDependencies}`
-    ].join(' ');
+    const installDependencies = dependencies && `${install} ${dependencies}` || `${install} ${devDependencies}`;
 
     console.log(installDependencies);
-    !folderContent.has(node_modules) && await bash(installDependencies, bashOptions);
+    await bash(installDependencies, bashOptions);
 }
